fe513bff3810e131291d74c9f549e257109653c3
kiv1n <aloha@kiv1n.ru>
2023-07-02 12:21:21 +0300

    Add script to deploy data supplier contract

---
+++ contracts/scripts/deploy/dataSupplier.ts
@@ -0,0 +1,20 @@
+import { ethers } from "hardhat";
+import { DataSupplier__factory } from "../../typechain-types";
+
+async function main() {
+  console.log("👟 Start to deploy data supplier contract");
+
+  // Define contract deployer
+  const accounts = await ethers.getSigners();
+  const deployer = accounts[0];
+
+  // Deploy contract
+  const contract = await new DataSupplier__factory(deployer).deploy();
+  await contract.waitForDeployment();
+  console.log(`✅ Contract deployed to ${contract.target}`);
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
