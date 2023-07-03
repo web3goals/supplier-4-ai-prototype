96365dd8b332e582ec4fa56ee76b19739af7095b
kiv1n <aloha@kiv1n.ru>
2023-07-03 15:33:53 +0300

    Add particle wallets

--- app/pages/_app.tsx
+++ app/pages/_app.tsx
@@ -1,11 +1,18 @@
 import "@/styles/globals.css";
 import { ThemeProvider } from "@mui/material";
 import {
-  getDefaultWallets,
+  connectorsForWallets,
   lightTheme,
   RainbowKitProvider,
 } from "@rainbow-me/rainbowkit";
 import "@rainbow-me/rainbowkit/styles.css";
+import {
+  coinbaseWallet,
+  injectedWallet,
+  metaMaskWallet,
+  rainbowWallet,
+  walletConnectWallet,
+} from "@rainbow-me/rainbowkit/wallets";
 import { DialogProvider } from "context/dialog";
 import type { AppProps } from "next/app";
 import NextNProgress from "nextjs-progressbar";
@@ -16,16 +23,50 @@ import { getSupportedChains } from "utils/chains";
 import { configureChains, createConfig, WagmiConfig } from "wagmi";
 import { publicProvider } from "wagmi/providers/public";
 
+import { ParticleNetwork } from "@particle-network/auth";
+import { particleWallet } from "@particle-network/rainbowkit-ext";
+
+new ParticleNetwork({
+  projectId: process.env.NEXT_PUBLIC_PARTICLE_PROJECT_ID as string,
+  clientKey: process.env.NEXT_PUBLIC_PARTICLE_CLIENT_KEY as string,
+  appId: process.env.NEXT_PUBLIC_PARTICLE_APP_ID as string,
+});
+
 const { chains, publicClient, webSocketPublicClient } = configureChains(
   [...getSupportedChains()],
   [publicProvider()]
 );
 
-const { connectors } = getDefaultWallets({
-  appName: "Dataset Supplier",
-  projectId: process.env.NEXT_PUBLIC_WALLET_CONNECT_PROJECT_ID || "",
-  chains,
-});
+const particleWallets =
+  typeof window !== "undefined"
+    ? [
+        particleWallet({ chains, authType: "google" }),
+        particleWallet({ chains }),
+      ]
+    : [];
+
+const connectors = connectorsForWallets([
+  {
+    groupName: "Popular",
+    wallets: [
+      ...particleWallets,
+      injectedWallet({ chains }),
+      rainbowWallet({
+        chains,
+        projectId: process.env.NEXT_PUBLIC_WALLET_CONNECT_PROJECT_ID as string,
+      }),
+      coinbaseWallet({ appName: "Dataset Supplier", chains }),
+      metaMaskWallet({
+        chains,
+        projectId: process.env.NEXT_PUBLIC_WALLET_CONNECT_PROJECT_ID as string,
+      }),
+      walletConnectWallet({
+        chains,
+        projectId: process.env.NEXT_PUBLIC_WALLET_CONNECT_PROJECT_ID as string,
+      }),
+    ],
+  },
+]);
 
 const config = createConfig({
   autoConnect: true,
